ed25505e77098b10138d87075147af60936ce6e1
abhii371 <abhisheknale1709@gmail.com>
2024-06-19 00:00:07 +0530

    dhd

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span style={{ fontSize: "2rem" }}>Minimal</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/devbhi/minimal",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "",
   },
-}
+  primaryHue: 278,
+};
 
-export default config
+export default config;
